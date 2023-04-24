284770c54c44bd259ee0e7696061522408b99433
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-24 14:52:05 +0530

    Added Authentication Exception Handling
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- App.java
+++ App.java
@@ -1,15 +1,19 @@
 class App {
     public static void main(String args[]) throws Exception {
         Authentication auth = new Authentication();
-        int valid = auth.ValidateUser();
-        if (valid == 1) {
-            System.out.println("ROOT ACCESS GRANTED");
-            System.out.println("***********************************Welcome to Bank Online Banking System*************************************");
 
-            Service s = new Service();
-            s.menu();
-        } else {
+        try {
+            int valid = auth.ValidateUser();
+            if (valid == 1) {
+                System.out.println("ROOT ACCESS GRANTED");
+                System.out.println("***********************************Welcome to Bank Online Banking System*************************************");
+    
+                Service s = new Service();
+                s.menu();
+            }
+        } catch (Exception e) {
             System.out.println("Invalid login credentials");
+            System.out.println(e);
         }
     }
 }
