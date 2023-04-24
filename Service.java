9c7e052ba75f40b89df4e40801dfbcdb4f602fc4
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-24 21:36:49 +0530

    Updated Readme.md
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Service.java
+++ Service.java
@@ -2,83 +2,87 @@ import java.io.*;
 
 class Service {
 	public void menu() throws Exception {
-
-		try {
-			while (true) {
-
-				System.out.println();
-
-				System.out.println("Enter 1 to Create New Account.");
-				System.out.println("Enter 2 for Deposit.");
-				System.out.println("Enter 3 for Withdrawl.");
-				System.out.println("Enter 4 for MiniStatement");
-				System.out.println("Enter 5 for Balance Enquiry.");
-				System.out.println("Enter 6 for Changing Password.");
-				System.out.println("Enter 7 for Account Details.");
-				System.out.println("Enter 8 to  Exit.");
-
-				System.out.println();
-
-				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-				System.out.println("Enter your Choice.");
-				int choice = Integer.parseInt(br.readLine());
-
-				UserAccount userAcc = new UserAccount();
-				Transaction trans = new Transaction();
-
-				switch (choice) {
-					case 1:
+		outer:
+		while (true) { 
+			try {
+				while (true) {
+					
+					System.out.println();
+					
+					System.out.println("Enter 1 to Create New Account.");
+					System.out.println("Enter 2 for Deposit.");
+					System.out.println("Enter 3 for Withdrawl.");
+					System.out.println("Enter 4 for MiniStatement");
+					System.out.println("Enter 5 for Balance Enquiry.");
+					System.out.println("Enter 6 for Changing Password.");
+					System.out.println("Enter 7 for Account Details.");
+					System.out.println("Enter 8 to  Exit.");
+					
+					System.out.println();
+					
+					BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+					
+					System.out.println("Enter your Choice.");
+					int choice = Integer.parseInt(br.readLine());
+					
+					UserAccount userAcc = new UserAccount();
+					Transaction trans = new Transaction();
+					
+					switch (choice) {
+						case 1:
 						userAcc.createAccount();
 						break;
-					case 2:
+						case 2:
 						trans.deposit();
 						break;
-					case 3:
+						case 3:
 						trans.withdrawal();
 						break;
-					case 4:
+						case 4:
 						trans.ministatement();
 						break;
-					case 5:
+						case 5:
 						trans.balcheck();
 						break;
 					case 6:
 						userAcc.changepass();
 						break;
-					case 7:
+						case 7:
 						userAcc.AccDetails();
 						break;
-					case 8:
+						case 8:
 						System.exit(0);
-						break;
-					default:
+						break outer;
+						default:
 						System.out.println("Enter Choice Correctly");
+					}
 				}
+			} catch (createAccountException e) {
+				System.out.println(e);
+				e.getMessage();
+				System.out.println("Service.java");
+			} catch (AccountDetailsException e) {
+				System.out.println(e);
+				e.getMessage();
+				System.out.println("Service.java");
+			} catch (AuthenticationException e) {
+				System.out.println(e);
+				e.getMessage();
+				System.out.println("Service.java");
+			} catch (DepositException e) {
+				System.out.println(e);
+				e.getMessage();
+				System.out.println("Service.java");
+			} catch (WithdrawalException e) {
+				System.out.println(e);
+				e.getMessage();
+			} catch (Exception e) {
+				System.out.println("Service.java");
+				System.out.println(e);
 			}
-		} catch (createAccountException e) {
-			System.out.println(e);
-			e.getMessage();
-			System.out.println("Service.java");
-		} catch (AccountDetailsException e) {
-			System.out.println(e);
-			e.getMessage();
-			System.out.println("Service.java");
-		} catch (AuthenticationException e) {
-			System.out.println(e);
-			e.getMessage();
-			System.out.println("Service.java");
-		} catch (DepositException e) {
-			System.out.println(e);
-			e.getMessage();
-			System.out.println("Service.java");
-		} catch (WithdrawalException e) {
-			System.out.println(e);
-			e.getMessage();
-		} catch (Exception e) {
-			System.out.println("Service.java");
-			System.out.println(e);
+			
 		}
 
+		System.out.println("Exit Bank ----------------------------- ");
 	}
 }
--- Transaction.java
+++ Transaction.java
@@ -48,8 +48,12 @@ class Transaction {
                 accountNumber = br.readLine();
                 System.out.println("Enter Amount to be deposited: ");
                 depositAmount = Double.parseDouble(br.readLine());
+
+                if (depositAmount <= 0) {
+                    throw new DepositException("Deposit Amount Invalid");
+                }
             } catch (Exception e) {
-                throw new DepositException("Error Taking input");
+                throw new DepositException("Error Taking input: " + e.getMessage());
             }
 
             try {
@@ -73,9 +77,9 @@ class Transaction {
             int tid = 0;
 
             try {
-                String q1 = "select tid,balance from bank_transaction where accno='" + accountNumber
+                String query1 = "select tid,balance from bank_transaction where accno='" + accountNumber
                         + "' and tid=(select max(tid) from bank_transaction where accno=" + accountNumber + ")";
-                ResultSet rs = StateMent.executeQuery(q1);
+                ResultSet rs = StateMent.executeQuery(query1);
                 tid = 0;
                 balance = 0;
                 if (rs.next()) {
@@ -96,11 +100,13 @@ class Transaction {
 
             balance = balance + depositAmount;
 
-            String q2 = "insert into bank_transaction values('" + tid + "','" + accountNumber + "','" + depositAmount + "','" + wamt
+            String query2 = "insert into bank_transaction values('" + tid + "','" + accountNumber + "','"
+                    + depositAmount
+                    + "','" + wamt
                     + "','"
                     + dt + "','" + balance + "')";
 
-            int t1 = StateMent.executeUpdate(q2);
+            int t1 = StateMent.executeUpdate(query2);
 
             if (t1 > 0) {
                 System.out.println("Deposited Successfully: ");
@@ -115,41 +121,116 @@ class Transaction {
 
     void withdrawal() throws WithdrawalException {
 
+        Connect Cn = null;
+        Connection Con = null;
+        Statement StateMent = null;
+
         try {
 
-            System.out.println("Enter Account No. to Withdrawal: ");
-            int accountNumber = Integer.parseInt(br.readLine());
-            System.out.println("Enter Amount to be Withdrawal: ");
-            double wamt = Double.parseDouble(br.readLine());
-            Connect Cn = new Connect();
-            Connection Con = Cn.fetchcon();
-            Statement StateMent = Con.createStatement();
-            String q1 = "select tid,balance from bank_transaction where accno='" + accountNumber
-                    + "' and tid=(select max(tid) from bank_transaction where accno=" + accountNumber + ")";
-            ResultSet rs = StateMent.executeQuery(q1);
+            int accountNumber = 0;
+            double wamt;
+
+            try {
+                System.out.println("Enter Account No. to Withdrawal: ");
+                accountNumber = Integer.parseInt(br.readLine());
+                System.out.println("Enter Amount to be Withdrawal: ");
+                wamt = Double.parseDouble(br.readLine());
+
+                if (wamt <= 0) {
+                    throw new DepositException("Invalid Withdraw Amount");
+                }
+            } catch (DepositException e) {
+                System.out.println(e);
+                throw new DepositException(e.getMessage());
+            } catch (Exception e) {
+                System.out.println(e);
+                throw new DepositException(e.getMessage());
+            }
+
+            // tryConnecting to db
+            try {
+
+                Cn = new Connect();
+                Con = Cn.fetchcon();
+                StateMent = Con.createStatement();
+
+            } catch (SQLException e) {
+
+                System.out.println("Error Connecting to the db");
+                System.out.println(e);
+
+                throw new WithdrawalException("Can't Connect");
+            } catch (Exception e) {
+
+                System.out.println(e);
+                System.out.println("Unknown error.....");
+
+                throw new WithdrawalException("Can't Connect");
+            }
+
             int tid = 0;
             double balance = 0;
-            if (rs.next()) {
-                tid = rs.getInt(1);
-                balance = rs.getDouble(2);
+
+            try {
+                String query1 = "select tid,balance from bank_transaction where accno='" + accountNumber
+                        + "' and tid=(select max(tid) from bank_transaction where accno=" + accountNumber + ")";
+                ResultSet rs = StateMent.executeQuery(query1);
+
+                if (rs.next()) {
+                    tid = rs.getInt(1);
+                    balance = rs.getDouble(2);
+                }
+            } catch (SQLException e) {
+                System.out.println(e);
+                throw new WithdrawalException("Error Executing Query1");
+            } catch (Exception e) {
+                System.out.println(e);
+                throw new WithdrawalException("Unknown Error in Withdrawl");
             }
-            if (balance - wamt >= 500) {
-                double depositAmount = 0;
-                Date date = new Date();
-                String dt = String.format("%tc", date);
-                balance = balance - wamt;
-                String q2 = "insert into bank_transaction values('" + tid + "','" + accountNumber + "','" + depositAmount + "','" + wamt
+
+            if (balance - wamt <= 500) {
+                System.out.println("Cannot Withdraw money");
+                throw new WithdrawalException("Insufficient Balance");
+            }
+
+            double depositAmount = 0;
+
+            Date date = new Date();
+            String dt = String.format("%tc", date);
+
+            balance = balance - wamt;
+
+            int t1 = 0;
+            try {
+
+                String query2 = "insert into bank_transaction values('" + tid + "','" + accountNumber + "','"
+                        + depositAmount + "','" + wamt
                         + "','" + dt + "','" + balance + "')";
-                int t1 = StateMent.executeUpdate(q2);
-                if (t1 > 0)
-                    System.out.println("Debited Successfully: ");
-            } else
-                System.out.println("INSUFFICENT BALANCE: ");
+                t1 = StateMent.executeUpdate(query2);
+
+            } catch (SQLException e) {
+                System.out.println(e);
+                throw new WithdrawalException("Error Executing Query2");
+            } catch (Exception e) {
+                System.out.println(e);
+                throw new WithdrawalException("Unknown Error in Withdrawl");
+            }
+
+            if (t1 > 0) {
+                System.out.println("Debited Successfully: ");
+            }
+
             Con.close();
 
         } catch (Exception e) {
             System.out.println(e);
             throw new WithdrawalException("Error: " + e.getMessage());
+        } finally {
+            try {
+                Con.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
         }
     }
 
@@ -160,9 +241,9 @@ class Transaction {
         Connection Con = Cn.fetchcon();
         Statement StateMent = Con.createStatement();
         System.out.println("TID\tAccno\tCREDIT\t\tDEBIT\t\t\tDATE\t\t\t\t\tBALANCE");
-        String q1 = "select * from bank_transaction where accno=" + accountNumber;
+        String query1 = "select * from bank_transaction where accno=" + accountNumber;
 
-        ResultSet rs = StateMent.executeQuery(q1);
+        ResultSet rs = StateMent.executeQuery(query1);
 
         File f = null;
         FileOutputStream os = null;
@@ -179,8 +260,9 @@ class Transaction {
             os = new FileOutputStream(f, true);
 
             while (rs.next()) {
-                String transactionEntry = rs.getInt(1) + "\t" + rs.getInt(2) + "\t" + rs.getDouble(3) + "\t" + "\t" + "\t" + rs.getDouble(4) + "\t" + rs.getString(5) + "\t" + rs.getDouble(6);
-                
+                String transactionEntry = rs.getInt(1) + "\t" + rs.getInt(2) + "\t" + rs.getDouble(3) + "\t" + "\t"
+                        + "\t" + rs.getDouble(4) + "\t" + rs.getString(5) + "\t" + rs.getDouble(6);
+
                 os.write(transactionEntry.getBytes());
 
                 System.out.println(transactionEntry);
@@ -210,9 +292,9 @@ class Transaction {
         Connect Cn = new Connect();
         Connection Con = Cn.fetchcon();
         Statement StateMent = Con.createStatement();
-        String q1 = "select tid,balance from bank_transaction where accno=" + accountNumber
+        String query1 = "select tid,balance from bank_transaction where accno=" + accountNumber
                 + " and tid=(select max(tid) from bank_transaction where accno=" + accountNumber + ")";
-        ResultSet rs = StateMent.executeQuery(q1);
+        ResultSet rs = StateMent.executeQuery(query1);
         double balance = 0;
         if (rs.next())
             balance = rs.getDouble(2);
